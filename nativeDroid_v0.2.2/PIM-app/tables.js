496b8b1d3af74e774962eabaeec8f8f36d8fe336
apoclyps <kyle90adam@hotmail.com>
2014-03-07 19:32:12 +0000

    Dynamically generated table updated to display CSS correctly.
    Some minor changes made to jquery css file to allow for columns to be displayed in the center of the UI instaed of floating to the right and blocking a table header

--- nativeDroid_v0.2.2/PIM-app/tables.js
+++ nativeDroid_v0.2.2/PIM-app/tables.js
@@ -8,31 +8,21 @@ $.ajax({
     contentType: "application/json",
     dataType: 'jsonp',
     success: function (data) {
-        var head = "<table data-role='table' id='table-column-toggle' data-mode='columntoggle' class='ui-responsive table-stroke' id='sub-table'>";
-
-        var tr = "<thead><tr><div class='row'><strong>";
-        var th0 = "<td><div class ='span1' style='padding-left:50px'> </div></td>";
-        var th1 = "<td><div class ='span3'> Name </div></td>";
-        var th2 = "<td><div class ='span2' style='padding-right:25px'> Issue Count </div></td>";
-        var th3 = "<td><div class ='span4' > Last Issue</div></td>";
-        var th4 = "<td><div class ='span1'> Last Issue Number</div></td>";
-        var th5 = "<td><div class ='span1'> Resource Type</div></td></strong></div></tr>";
-
-        $("#table-column-toggle").append(tr + th0 + th1 + th2 + th3 + th4 + th5 + "</thead><tbody>");
 
         for (var i in data.COMICVINE) {
-            var tr = "<tr><div class='row'><a href='#'>";
-            var td0 = "<th><div class ='span1'>"+i+"</div></th>";
-            var td1 = "<td><div class ='span3'>" + data.COMICVINE[i].name + "</div></td>";
-            var td2 = "<td><div class ='span2' style='padding-right:50px'>" + data.COMICVINE[i].count_of_issues + "</div></td>";
-            var td3 = "<td><div class ='span4'>" + data.COMICVINE[i].last_issue.name + "</div></td>";
-            var td4 = "<td><div class ='span1'>" + data.COMICVINE[i].last_issue.issue_number + "</div></td>";
-            var td5 = "<td><div class ='span1'>" + data.COMICVINE[i].resource_type + "</div></td></div></a></tr>";
+		    var rank = i.valueOf();
+			rank++;
+            var tr = "<tr>";
+            var td0 = "<th class='class='ui-table-priority-2 ui-table-cell-visible'>"+rank+"</th>";
+            var td1 = "<td class='title'><a href='#' class='ui-link' data-rel='external'>" + data.COMICVINE[i].name + "</a></td>";
+            var td2 = "<td class='ui-table-priority-3 ui-table-cell-visible'>" + data.COMICVINE[i].count_of_issues + "</td>";
+            var td3 = "<td class='ui-table-priority-1 ui-table-cell-visible'>" + data.COMICVINE[i].last_issue.name + "</td>";
+            var td4 = "<td class='ui-table-priority-4 ui-table-cell-visible' style='padding-right:30px'>" + data.COMICVINE[i].last_issue.issue_number + "</td></tr>";
 
             $("#table-column-toggle").append(tr +td0 + td1 + td2 + td3 + td4 );
         }
-        $("#table-column-toggle").append("</tbody></table></div>");
-		//$('#table').table('refresh');
+
+		$("#table-column-toggle").table("refresh");
 		console.log("Request Complete : Comic Vine Volume");
     },
     error: function (e) {

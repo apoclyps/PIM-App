af7120f95afc450cd1969fc1d8974d5fa28d9d64
apoclyps <kyle90adam@hotmail.com>
2014-03-29 14:55:40 +0000

    Login and Register Javascript seperated.
    Login.html now hits the servers and checks if an account exists. Returrns true or false in the data which redirects if true.

--- nativeDroid_v0.2.2/PIM-app/ajax/register.js
+++ nativeDroid_v0.2.2/PIM-app/ajax/register.js
@@ -1,4 +1,4 @@
-$('#login').submit(function () {
+$('#register').submit(function () {
 	sendAjax();
 	return false;
 });
@@ -9,13 +9,10 @@ function sendAjax() {
 	var login = new Object();
 	login.username = $('#username').val();
 	login.password = $('#password').val();
-	
-	var externalServer = "http://137.117.146.199:8080/PIM-Server/register";
-	var localServer = "http://127.0.0.1:8080/PIM-Server/login";
 
 	jQuery.support.cors = true;
 	$.ajax({
-		url: localServer,
+		url: "http://137.117.146.199:8080/PIM-Server/register",
 		type: 'POST',
 		dataType: 'jsonp',
 		data: JSON.stringify(login),
@@ -24,11 +21,9 @@ function sendAjax() {
 		mimeType: 'application/json',
 		
 		success: function (data) {
-		alert(data.success);
+		alert(data.username);
 		console.log(data.username);
-		if(data.success){
-			window.location.href = "index.html";
-		}
+		window.location.href = "index.html";
         },
 		error:function(data,status,er) {
 			console.log("error: "+data+" status: "+status+" er:"+er);

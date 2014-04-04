3dc48617f3b847d042f4cd5955094b8a5a07ace1
apoclyps <kyle90adam@hotmail.com>
2014-04-04 15:33:15 +0100

    Listview updated with loading icon and success message when loading is complete
    Additional icons added to package
    
    Added updated Jquery and Jquery mobile to package. Needs to be tested before all web pages are changed to these newer versions.

--- nativeDroid_v0.2.2/PIM-app/ajax/listview.js
+++ nativeDroid_v0.2.2/PIM-app/ajax/listview.js
@@ -1,7 +1,13 @@
 //var url = 'http://192.168.0.17:8080/PIM-Server/comicvine?callback=?';
 $(document).ready(function () {
     //console.log( "Document ready : listview.js");
-    //$.mobile.loading('show')
+    //$.mobile.loading('show');
+     $('#message-info').hide();
+
+     $( "#overlay" ).fadeIn( "slow", function() {
+    // Animation complete
+    });
+
 
     //var url = 'http://127.0.0.1:8080/PIM-Server/comicvine?callback=?';
     var url = 'http://137.117.146.199:8080/PIM-Server/comicvine?callback=?&query=batman';
@@ -15,7 +21,6 @@ $(document).ready(function () {
         success: function (data) {
 
             for (var i in data.COMICVINE) {
-
                 var last_issue = null;
 
                 if (data.COMICVINE[i].last_issue.name == null) {
@@ -31,18 +36,14 @@ $(document).ready(function () {
                     '<p class="ui-li-aside"><strong>' + data.COMICVINE[i].count_of_issues + '</strong> Issues</p>' +
                     '</a></li>';
                 $("#comicview").append(appendString);
-                $('#comicview').listview('refresh');
-
-
+                
                 //Creating a datastore for ComicvineData
                 var dataToStore = JSON.stringify(data.COMICVINE[i]);
                 // Storing data in localstorage 
                 window.localStorage.setItem(data.COMICVINE[i].id, dataToStore);
-
-                //var key = data.COMICVINE[i].id;
-                //var localData = JSON.parse(localStorage.getItem(key));
             }
-
+             $('#comicview').listview('refresh');
+             $('#overlay').fadeOut();
 
             //Creating a datastore for ComicvineData
             var dataToStore = JSON.stringify(data.COMICVINE[0]);
@@ -57,81 +58,26 @@ $(document).ready(function () {
                 alert("Value is null");
             } else {
                 //alert(localData.toString());
-                alert("Accessed from LocalStorage : " + localData.id);
+                //alert("Accessed from LocalStorage : " + localData.id);
             }
 
             // localStorage is now empty
             //window.localStorage.clear();
 
-
             $('#comicview').listview('refresh');
             //console.log("Request Complete : Comic Vine Volume");
-            //$.mobile.loading('hide')
+            //$.mobile.loading('hide');
+
+            $( "#message-info" ).fadeIn( "slow", function() {
+            // Animation complete
+            });
         },
         error: function (e) {
             console.log(e.message);
+            $('#overlay').fadeOut();
+            alert("Server could not be reached");
         }
     });
-
-
-    //SQL LITE	
-    // Wait for device API libraries to load
-    //
-    document.addEventListener("deviceready", onDeviceReady, false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-        //var db = window.openDatabase("Database", "1.0", "PIM", 200000);
-        //db.transaction(createDB, errorCB, successCB);
-        // db.transaction(populateDB, errorCB, successCB);
-        //db.transaction(queryDB, errorCB);
-    }
-
-    // Populate the database
-    //
-
-    function createDB(tx) {
-        tx.executeSql('DROP TABLE IF EXISTS COMICS');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS COMICS (id unique, data)');
-    }
-
-    function populateDB(tx) {
-        tx.executeSql('INSERT INTO COMICS (id, data) VALUES (1, "First row")');
-        tx.executeSql('INSERT INTO COMICS (id, data) VALUES (2, "Second row")');
-        //alert(tx.executeSql('SELECT count(*) FROM COMICS'));
-    }
-
-    // Transaction error callback
-    //
-    function errorCB(tx, err) {
-        alert("Error processing SQL: " + err);
-    }
-
-    // Transaction success callback
-    //
-    function successCB() {
-        alert("success!");
-    }
-
-    function queryDB(tx) {
-        tx.executeSql('SELECT * FROM COMICS', [], querySuccess, errorCB);
-    }
-
-    function querySuccess(tx, results) {
-        alert("Returned rows = " + results.rows.length);
-        // this will be true since it was a select statement and so rowsAffected was 0
-        if (!results.rowsAffected) {
-            alert('No rows affected!');
-            return false;
-        }
-        // for an insert statement, this property will return the ID of the last inserted row
-        alert("Last inserted row ID = " + results.rows.item(0).id);
-    }
-
-    function errorCB(err) {
-        alert("Error processing SQL: " + err.code);
-    }
 
 
 });

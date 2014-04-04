c4da38fdba5cf2af0342cf44b4ee8f022f5d0ccf
apoclyps <kyle90adam@hotmail.com>
2014-04-04 21:26:56 +0100

    ZXCVBN tested passwordStrength.js and updates accordingly based on password scoring from insecure to secure.
    Images icons added also.

---
+++ nativeDroid_v0.2.2/PIM-app/ajax/passwordStrength.js
@@ -0,0 +1,30 @@
+$(window).load(function () {
+    $('#password').keyup(function () {
+            
+    var textValue = $(this).val();
+    var result = zxcvbn(textValue);
+
+    var currentScore = result.score;
+    var imageFile = null;
+
+    var passwordStrength = null;
+    if (currentScore == 0) {
+        imageFile = "insecure.png";
+        passwordStrength = "Insecure";
+    } else if (currentScore == 1) {
+        imageFile = "warning.png";
+        passwordStrength ="Weak"
+    } else if(currentScore == 2){
+        imageFile = "warning.png";
+        passwordStrength ="Average"
+    }else if (currentScore == 3) {
+        imageFile = "secure.png";
+        passwordStrength ="Strong"
+    }else if (currentScore >= 4) {
+        imageFile = "secure.png";
+        passwordStrength ="Secure"
+    }
+    $('#result').html('Password Strengh : '+passwordStrength);
+    var injectHTML = "<p><img src='images/"+imageFile+"'></img></p>";
+    document.getElementById("strength").innerHTML = injectHTML
+}); 
